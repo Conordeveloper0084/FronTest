9c5effc749d322fb4ee177bbc6a1129185367d8d
Conordeveloper0084 <conordev0084@gmail.com>
2026-01-04 12:25:51 +0500

    Updaetd

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 app = FastAPI()
 
-# static fayllar
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(BASE_DIR, "static")),
+    name="static",
+)
 
 @app.get("/")
 def root():
-    return FileResponse("static/index.html")
+    return FileResponse(os.path.join(BASE_DIR, "static", "index.html"))
