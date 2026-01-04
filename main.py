56da9717a488ada9bb79ce33eed66ce776f3decd
Conordeveloper0084 <conordev0084@gmail.com>
2026-01-04 12:29:42 +0500

    Udpated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,6 +13,8 @@ app.mount(
     name="static",
 )
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def root():
-    return FileResponse(os.path.join(BASE_DIR, "static", "index.html"))
+    index_path = os.path.join(BASE_DIR, "static", "index.html")
+    with open(index_path, "r", encoding="utf-8") as f:
+        return f.read()
